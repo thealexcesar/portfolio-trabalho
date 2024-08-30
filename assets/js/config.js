9fce1ec9a0dd1001baa5537c1e0c2cce166cee3c
Alex Cesar da Silva <thealexcesar@gmail.com>
2024-08-30 16:15:25 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,1 @@
-// Em aplicacao real, colocaria este arquivo no meu .gitignore e .dockerignore
-const CONFIG = {
-    SERVICE_ID: 'service_lg5vujv',
-    PUBLIC_KEY: 'wYgPq3VnJDzzBE8lQ',
-    TEMPLATE_ID: 'template_xdtdqpv'
-};
+
